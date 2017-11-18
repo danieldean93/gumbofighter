6ef9888a23c0fa9c26a316bfbb9bfe927f26eabb
DESKTOP-KVJNAPG\toddm <toddmoulder88@gmail.com>
2017-11-18 11:17:04 -0600

    aiships

---
+++ GUMBOFIGHTER/Source/GUMBOFIGHTER/AIShipController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "AIShipController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GUMBOFIGHTER_API AAIShipController : public AAIController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
